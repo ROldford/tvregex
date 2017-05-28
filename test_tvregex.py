e7cc1b82cb5a261d6909da3d1d35a8e1b8f99a8f
Ryan William Maynard Oldford <ryan.oldford@gmail.com>
2017-05-28 09:08:30 +0700

    Added renamed filenames, changed order again

--- test_tvregex.py
+++ test_tvregex.py
@@ -15,20 +15,18 @@ import tvregex as tvr
 class TestIntegration(unittest.TestCase):
 
     def test_integration_silent(self):
-        # TODO: Enter raw filenames
         raw_filenames = [
-            "Archer.2009.S08E08.REPACK.HDTV.x264-SVA.mkv",  # Archer
             "lip.sync.battle.s03e19.hdtv.x264-w4f.mkv",  # Lip Sync Battle
+            "Archer.2009.S08E08.REPACK.HDTV.x264-SVA.mkv",  # Archer
             "The.Daily.Show.2017.03.13.Lee.Daniels.720p.CC.WEBRip.AAC2.0.x264-BTW.mkv",  # Daily Show
             "at.midnight.2017.02.14.hdtv.x264-crooks.mkv"  # @midnight
         ]
         silent = True
-        # TODO: Enter matching correct filenames
         correct_filenames = [
-            "",  # Archer
-            "",  # Lip Sync Battle
-            "",  # Daily Show
-            "",  # @midnight
+            "Lip Sync Battle - [03x19].mkv",  # Lip Sync Battle
+            "Archer (2009) - [08x08].mkv",  # Archer
+            "The Daily Show - [2017-03-13].mkv",  # Daily Show
+            "@midnight - [2017-02-14].mkv",  # @midnight
         ]
         for i in range(len(raw_filenames)):
             raw_filename = raw_filenames[i]

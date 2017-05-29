599e4ed15706d3846a5afa942e13d9aff15a42fc
Ryan Oldford <ryan.oldford@gmail.com>
2017-05-29 22:44:38 +0700

    Edited fix_episode() and tvregex() to pass integration tests

--- tvregex.py
+++ tvregex.py
@@ -3,7 +3,7 @@ import re
 
 def fix_episode_seasonal(episode):
     return_value = episode
-    pattern_string = r".??(\d{1,2}).??(\d{1,2})"
+    pattern_string = r"(?:s|\[)(\d{1,2})(?:e|x)(\d{1,2})"
     pattern = re.compile(pattern_string, flags=re.IGNORECASE)
     match = pattern.search(episode)
     if match:
@@ -31,7 +31,18 @@ def fix_episode_daily(episode):
 
 
 def fix_episode(episode):
-    return fix_episode_seasonal(episode)
+    return_value = episode
+    pattern_string_seasonal = r"(?:s|\[)(\d{1,2})(?:e|x)(\d{1,2})"
+    pattern_seasonal = re.compile(pattern_string_seasonal, flags=re.IGNORECASE)
+    match_seasonal = pattern_seasonal.search(return_value)
+    pattern_string_daily = r".*?(\d{4}).+?(\d{2}).+?(\d{2}).*?$"
+    pattern_daily = re.compile(pattern_string_daily)
+    match_daily = pattern_daily.search(return_value)
+    if match_seasonal:
+        return_value = fix_episode_seasonal(return_value)
+    elif match_daily:
+        return_value = fix_episode_daily(return_value)
+    return return_value
 
 
 def fix_title(showname, shownames_dict):
@@ -44,7 +55,11 @@ def fix_title(showname, shownames_dict):
 
 def tvregex(filename, shownames_dict):
     return_value = filename
-    pattern_string = r"(.+)\.(s\d{2}e\d{2}).+\.(.+)"
+    pattern_string = (
+        r"(.+)\." +
+        "((?:s\d{2}e\d{2})|(?:\d{4}\.\d{2}\.\d{2}))" +
+        ".+\.(.+)"
+    )
     pattern = re.compile(pattern_string, flags=re.IGNORECASE)
     match = pattern.search(filename)
     showname, episode, extension = match.groups()

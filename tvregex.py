ae83f6a8cc14936d294674eab4cca4a581f91cf1
Ryan Oldford <ryan.oldford@gmail.com>
2017-06-01 08:06:54 +0700

    Added --silent argument (doesn't yet do anything)

--- tvregex.py
+++ tvregex.py
@@ -62,7 +62,17 @@ def main():
         "file",
         help="Path to the file you want to rename"
     )
+    parser.add_argument(
+        "-s", "--silent",
+        help="Program will run without any messages" +
+        " (except unknown exceptions). " +
+        "(If set, this won't ask for matches)",
+        action="store_true"
+    )
     program_args = parser.parse_args()
+    silent = program_args.silent
+    if silent:
+        print("silent running")
     filepath = program_args.file
     if os.path.isfile(filepath):
         folder = os.path.dirname(filepath)
